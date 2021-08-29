8cc42b27462198118b018241eaa806f4648c91a4
jtnc101 <71797572+jtnc101@users.noreply.github.com>
2021-08-29 00:12:36 -0400

    add router with routes / and /search

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Main() {
+  return (
+    <div>
+      <Link to="/search">search</Link>
+      <div>App Placeholder</div>
+    </div>
+  );
+}
+
+export default Main;
