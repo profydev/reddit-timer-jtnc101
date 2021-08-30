82c796e23af374bc2b41e100fb068c5001c804e9
jtnc101 <71797572+jtnc101@users.noreply.github.com>
2021-08-30 00:29:59 -0400

    when clicking the links the default subreddit javascript is included in the URL

--- src/Hero.js
+++ src/Hero.js
@@ -4,15 +4,17 @@ import HeroCSS from './Hero.module.css';
 import table from './images/table.png';
 
 function Hero() {
+  const defaultSearch = 'javascript';
+
   return (
     <div className={HeroCSS.container}>
       <span className={HeroCSS.title}>No reactions to your reddit posts?</span>
       <span className={HeroCSS.subtitle}>
         Great timing, great results! Find the best time to post on your subreddit.
       </span>
-      <Link to="/search"><button type="button" className={HeroCSS.showMeButton}><span>SHOW ME THE BEST TIME</span></button></Link>
+      <Link to={`/search?value=${defaultSearch}`}><button type="button" className={HeroCSS.showMeButton}><span>SHOW ME THE BEST TIME</span></button></Link>
       <span className={HeroCSS.searchText}>r/javascript</span>
-      <Link to="/search"><img src={table} alt="table" className={HeroCSS.tableImage} /></Link>
+      <Link to={`/search?value=${defaultSearch}`}><img src={table} alt="table" className={HeroCSS.tableImage} /></Link>
     </div>
   );
 }
